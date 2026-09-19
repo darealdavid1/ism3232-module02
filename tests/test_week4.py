eb8d89e33f7f7e9bc9b083dbea363e1fea194167
David Leatherman <dl38@usf.edu>
2026-09-19 15:14:56 -0400

    lab 4: search ritual and git

--- tests/test_week4.py
+++ tests/test_week4.py
@@ -1,13 +1,11 @@
 def test_always_passes():
     assert 2 + 2 == 4
 
-
 def test_string_is_lowercase():
-    name = "ism3232"
+    name = 'ism3232'
     assert name == name.lower()
 
-
 def test_path_segments():
-    path = "/Users/yourname/ism3232/module02_zsh"
-    parts = path.split("/")
-    assert "ism3232" in parts
+    path = '/Users/yourname/ism3232/module02_zsh'
+    parts = path.split('/')
+    assert 'ism3232' in parts
